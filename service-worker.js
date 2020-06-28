4904dfdba95bf2955528e371c56523b7389faf28
DTR <t.terry.rice@gmail.com>
2020-06-28 18:00:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.cac2260acc2ae0fc6567e075e3b2b966.js"
 );
 
 self.addEventListener('message', (event) => {
